3ef22376d3e85549fc49c9b250e24051d58c1193
Pedro Alvarez <pedro.alvarez@softiron.com>
2023-11-27 13:52:53 +0000

    Improve lamadava script

--- roles/monitoring/templates/check-lamadava-balance.sh
+++ roles/monitoring/templates/check-lamadava-balance.sh
@@ -2,6 +2,7 @@
 
 export EAS_LAMADAVA_APIK={{ lamadava_token }}
 
+
 url="https://api.lamadava.com/sys/balance"
 api_key=${EAS_LAMADAVA_APIK}
 if [ -z "$api_key" ]; then
@@ -9,8 +10,28 @@ if [ -z "$api_key" ]; then
   exit 1
 fi
 
-balance=$(curl -s -G "$url" --data-urlencode "access_key=$api_key" | jq -r '.amount')
+response=$(curl -s -G "$url" --data-urlencode "access_key=$api_key")
+# Check if the curl command was successful
+if [ $? -ne 0 ]; then
+  echo "API call failed"
+  exit 1
+fi
+
+# Extract balance from the JSON response
+balance=$(echo "$response" | jq -r '.amount' 2>/dev/null)
+if [ -z "$balance" ]; then
+  echo "Failed to parse balance from API response"
+  exit 1
+fi
+
+# Remove the decimal part
 balance=${balance%.*}
+
+if ! [[ "$balance" =~ ^[0-9]+$ ]]; then
+  echo "Balance is not a valid integer: $balance"
+  exit 20
+fi
+
 if [ "$balance" -le 0 ]; then
   exit 1
 fi
